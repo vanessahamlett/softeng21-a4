cab8fa65bdcf63cc079990f11fb218116aa83f2f
vanessahamlett <vanessahamlett2000@gmail.com>
2021-10-10 15:41:33 -0500

    added base test names

---
+++ tests/__init__.py
@@ -0,0 +1,24 @@
+from pmgr.project import TaskException, Project
+import pytest
+
+def test_show():
+    pass
+
+def test_add_new():
+    pass
+
+def test_add_multiple_tasks():
+    pass
+
+def test_add_prexisting():
+    pass
+
+def test_delete_prexisting():
+    pass
+
+def test_delete_nonexisting():
+    pass
+
+def test_delete_multiple_tasks():
+    pass
+
